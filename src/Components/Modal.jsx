0c5c621af012db6886926e3cf0fe36b87f4fdd0f
Ngene Samuel Obasi <samuelobasi2005@gmail.com>
2022-06-12 19:53:39 +0100

    MODAL
    MODAL PERFECTED

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,34 +1,21 @@
 import React from "react";
 import { FaCheckCircle } from "react-icons/fa";
 import { MdCancel } from "react-icons/md";
+import "./Modal.css";
 
 const Modal = ({ closeModal }) => {
   return (
-    <div className="modalBackground">
-      <div className="modalContainer">
-        <button
-          className="closeModal"
-          onClick={() => {
-            closeModal(false);
-          }}
-        >
-          <MdCancel />
-        </button>
-        <div className="modalTitle">Message Sent!!</div>
-        <div className="modalBody">
-          <FaCheckCircle />
-        </div>
-        <div className="modalFooter">
-          <button
-            className="closeModal-large"
-            onClick={() => {
-              closeModal(false);
-            }}
-          >
-            Continue
-          </button>
-        </div>
+    <div className="modalContainer">
+      <div className="modalText">
+        <b>Thank You</b> <br /> Message Sent Sucessfully
       </div>
+      <button
+        onClick={() => {
+          closeModal(false);
+        }}
+      >
+        <MdCancel className="modalIcon" />
+      </button>
     </div>
   );
 };
