7b2a03f3908defbbb2247d0a91a5c1a71d1b15d4
Ngene Samuel Obasi <samuelobasi2005@gmail.com>
2022-07-08 07:34:30 +0100

    Added Animations

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "../App.css";
 import resume from "./RESUME.pdf";
+import "animate.css";
 
 const Home = () => {
   return (
     <div className="homeContainer">
-      <div className="hiThere">Hi There, I'm</div>
-      <div className="name">Ngene Samuel</div>
-      <div className="occupation">Web Developer</div>
+      <div className="animate__animated  animate__backInLeft hiThere">
+        Hi There, I'm
+      </div>
+      <div className="animate__animated animate__jackInTheBox name">
+        Ngene Samuel
+      </div>
+      <div className="animate__animated animate__backInRight occupation">
+        Web Developer
+      </div>
       <div className="resume">
         <a href={resume} download="download">
           DOWNLOAD MY CV
